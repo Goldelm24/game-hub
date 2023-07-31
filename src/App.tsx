c912eba63acc61667a3c87ee0a13d67f439d381d
Josias Guzman <jguzman@aoiths.org>
2023-07-31 17:26:28 -0400

    Navbar commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Grid templateAreas={{
-      base: `"nav" "main"`,
-      lg: `"nav nav" "aside main"`,  //1024px
-    }}>
-      <GridItem area="nav" bg="coral">
-        Nav
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"` //1024px
+      }}
+    >
+      <GridItem area="nav">
+        <Navbar/>
       </GridItem>
-      <Show above='lg'>
+      <Show above="lg">
         <GridItem area="aside" bg="gold">
           Aside
         </GridItem>
