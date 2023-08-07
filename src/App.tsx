0346dab318b6ed59f8765d1596631cce31889fb1
Josias Guzman <jguzman@aoiths.org>
2023-08-07 12:02:49 -0400

    Display genres

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,23 @@ function App() {
     <Grid
       templateAreas={{
         base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"` //1024px
+        lg: `"nav nav" "aside main"`, //1024px
+      }}
+      templateColumns={{
+        base: '1fr',
+        lg: '200px'
       }}
     >
       <GridItem area="nav">
-        <Navbar/>
+        <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
-          <GenreList/>
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
         </GridItem>
       </Show>
       <GridItem area="main">
-        <GameGrid/>
+        <GameGrid />
       </GridItem>
     </Grid>
   );
